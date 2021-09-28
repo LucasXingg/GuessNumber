869db47b4b91c05f884016aad76275b9c5f59421
LucasXing <xcl200212@gmail.com>
2021-09-29 00:13:24 +0100

    Body Finished!

---
+++ Game.py
@@ -0,0 +1,46 @@
+#导入包
+import random
+
+#初始化函数
+username = ''
+randomnum = 0
+inputnum = -1
+time = 1
+
+
+
+
+#开头
+print('Hello')
+#username = input('Please set your user name:')
+print('Hi ' +username+ ', here\'s a number between 0 and 100, let\'s Find out!')
+
+
+
+#body
+randomnum = random.randint(0,100)
+print(randomnum)
+while inputnum != randomnum :
+    while True:
+        inputnum = input('Let\'s Guess:')
+        try:
+            inputnum = int(inputnum)
+        except ValueError:
+            print('Please put a number here XD')
+        else:
+            break
+    
+    if inputnum < 0 or inputnum >100 :
+        print('This number is between 1-100')
+    else :
+        if inputnum > randomnum :
+            print('Try a smaller one')
+            time = time + 1
+        if inputnum < randomnum :
+            print('Try a bigger one')
+            time = time + 1
+print('Congratulation' +username+'! You Find out the number in ' +str(time)+ ' times')  #finish
+
+
+
+
